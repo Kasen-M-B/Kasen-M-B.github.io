a5fdda83a6663160f1d1aab955745b3722498f66
Kasen Benton <kasen.benton@gmail.com>
2026-08-23 00:17:31 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: "Kasen Benton",
-  title: "MIS Student | Aspiring Systems Analyst | hobbist video game developer",
+  title: "MIS Student | Aspiring Systems Analyst | hobbyist video game developer",
   description: "Portfolio website of Kasen Benton",
   accentColor: "#FF7300",
 
